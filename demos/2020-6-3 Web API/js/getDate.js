05852e3bc45d5711ad90d0938c68751030d394da
aoding9 <xy5217733@163.com>
2020-06-03 17:39:59 +0800

    6-3-1webapi基础学习ing

---
+++ demos/2020-6-3 Web API/js/getDate.js
@@ -0,0 +1,9 @@
+function getDate() {
+    var date = new Date(),
+    y = date.getFullYear(),
+    m = date.getMonth(),
+    d = date.getDate(),
+    weekendArr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+    weekend = date.getDay();
+    return y + '年 ' + m + '月' + d + '日 ' + weekendArr[weekend];
+}
