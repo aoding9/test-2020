a9e4c3ec81a2fb3e8a94118cffbd47fec97a0e9f
aoding9 <xy5217733@163.com>
2020-10-15 17:51:08 +0800

    10-15vue前后端交互 promise fetchAPI axios async await

--- demos/2020-9-28 Vue继续学/day4/myapi/index.js
+++ demos/2020-9-28 Vue继续学/day4/myapi/index.js
@@ -3,26 +3,28 @@ const app = express()
 const bodyParser = require('body-parser')
 // 处理静态资源
 app.use(express.static('public'))
-// 处理参数
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// 处理请求参数，支持json格式和query字符串格式
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // 设置允许跨域访问该服务
 app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Headers', 'mytoken');
-  next();
-});
+  // 允许跨域
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  // 跨域允许携带cookie mytoken  允许这些请求头字段
+  res.header('Access-Control-Allow-Headers', ['X-Requested-With', 'Content-Type', 'mytoken'])
+  // res.header('Access-Control-Allow-Headers', 'Content-Type');
+  // res.header('Access-Control-Allow-Headers', 'mytoken');
+  next()
+})
 app.get('/async1', (req, res) => {
   res.send('hello1')
 })
 app.get('/async2', (req, res) => {
-  if(req.query.info == 'hello') {
+  if (req.query.info == 'hello') {
     res.send('world')
-  }else{
+  } else {
     res.send('error')
   }
 })
@@ -31,9 +33,11 @@ app.get('/adata', (req, res) => {
   res.send('Hello axios!')
 })
 app.get('/axios', (req, res) => {
+  // query字符串传参 用req.query
   res.send('axios get 传递参数' + req.query.id)
 })
 app.get('/axios/:id', (req, res) => {
+  // restful传参 用req.params
   res.send('axios get (Restful) 传递参数' + req.params.id)
 })
 app.delete('/axios', (req, res) => {
@@ -47,12 +51,12 @@ app.put('/axios/:id', (req, res) => {
 })
 
 app.get('/axios-json', (req, res) => {
+  // res.json()响应json数据
   res.json({
     uname: 'lisi',
-    age: 12
-  });
+    age: 12,
+  })
 })
-
 
 app.get('/fdata', (req, res) => {
   res.send('Hello Fetch!')
@@ -67,6 +71,7 @@ app.delete('/books/:id', (req, res) => {
   res.send('DELETE请求传递参数!' + req.params.id)
 })
 app.post('/books', (req, res) => {
+  // post数据通过bodyparser的body获取到
   res.send('POST请求传递参数!' + req.body.uname + '---' + req.body.pwd)
 })
 app.put('/books/:id', (req, res) => {
@@ -77,24 +82,24 @@ app.get('/json', (req, res) => {
   res.json({
     uname: 'lisi',
     age: 13,
-    gender: 'male'
-  });
+    gender: 'male',
+  })
 })
 
 app.get('/a1', (req, res) => {
-  setTimeout(function(){
+  setTimeout(function () {
     res.send('Hello TOM!')
-  },1000);
+  }, 1000)
 })
 app.get('/a2', (req, res) => {
-  setTimeout(function(){
+  setTimeout(function () {
     res.send('Hello JERRY!')
-  },2000);
+  }, 2000)
 })
 app.get('/a3', (req, res) => {
-  setTimeout(function(){
+  setTimeout(function () {
     res.send('Hello SPIKE!')
-  },3000);
+  }, 3000)
 })
 
 // 路由
@@ -102,9 +107,9 @@ app.get('/data', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/data1', (req, res) => {
-  setTimeout(function(){
+  setTimeout(function () {
     res.send('Hello TOM!')
-  },1000);
+  }, 1000)
 })
 app.get('/data2', (req, res) => {
   res.send('Hello JERRY!')
