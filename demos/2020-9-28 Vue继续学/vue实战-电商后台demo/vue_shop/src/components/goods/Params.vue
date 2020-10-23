d7956aefa8469a8785ee5a80b558ca5968d3cadc
aoding9 <xy5217733@163.com>
2020-10-23 18:26:04 +0800

    10-23商品参数中

---
+++ demos/2020-9-28 Vue继续学/vue实战-电商后台demo/vue_shop/src/components/goods/Params.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb></el-breadcrumb>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
