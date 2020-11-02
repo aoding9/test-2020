d12047b678a2b16a8385e9568bc71a79947998ec
aoding9 <xy5217733@163.com>
2020-11-02 22:51:30 +0800

    11-2Vuex完成

--- demos/2020-9-28 Vue继续学/vuex/vuex-demo2/src/App.vue
+++ demos/2020-9-28 Vue继续学/vuex/vuex-demo2/src/App.vue
@@ -8,39 +8,59 @@
     />
     <a-button @click="addItemToList" type="primary">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="filterList" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{ item.info }}</a-checkbox>
+        <!-- 注意，此处@change事件的值不是普通函数，是一个回调函数，Function(e:Event)，先用外层函数接收e，然后再将e传给回调 -->
+        <a-checkbox
+          :checked="item.done"
+          @change="e => checkboxChangeHandle(e, item.id)"
+          >{{ item.info }}</a-checkbox
+        >
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <!-- 将id传给mutation -->
+        <a slot="actions" @click="removeById(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{ unDoneLength }}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button
+            @click="toChangeViewKey('all')"
+            :type="viewKey === 'all' ? 'primary' : 'default'"
+            >全部</a-button
+          >
+          <a-button
+            @click="toChangeViewKey('undone')"
+            :type="viewKey === 'undone' ? 'primary' : 'default'"
+            >未完成</a-button
+          >
+          <a-button
+            @click="toChangeViewKey('done')"
+            :type="viewKey === 'done' ? 'primary' : 'default'"
+            >已完成</a-button
+          >
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="toClean">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+// 这里为了体验两种写法，所以不都用辅助函数
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'app',
   data() {
     return {
       // * 将list数据放到public/list.json中，然后用action异步请求他，再作为参数传给mutation，在其中为state的list赋值保存
       // *然后通过this.$store.state.list拿到数据
+      // !通过getters过滤list，根据viewKey返回不同的item数组
     }
   },
   created() {
@@ -49,7 +69,9 @@ export default {
   },
   computed: {
     // 将store的list映射为组件自己的computed
-    ...mapState(['list', 'inputValue'])
+    // !为了用viewKey切换不同完成状态的list数组，将list改为getters处理，用filterList代替
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'filterList'])
   },
   methods: {
     // 将修改后的值传给mutation，修改state中的值
@@ -64,13 +86,35 @@ export default {
         return this.$message().warning('文本框内容不能为空~~~')
       }
       this.$store.commit('addItem')
-    }
-  },
-  watch: {
-    list() {
-      console.log(this.list)
+    },
+    // 通过id删除item，将id传给mutation
+    removeById(id) {
+      this.$store.commit('removeItem', id)
+    },
+    // 监听复选框change事件
+    checkboxChangeHandle(e, id) {
+      // 拿到选中状态和id值
+      // console.log(e.target.checked,id)
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    // 清除已完成
+    toClean() {
+      this.$store.commit('cleanDone')
+    },
+    // 切换viewKey
+    toChangeViewKey(key) {
+      this.$store.commit('changeViewKey', key)
     }
   }
+  // watch: {
+  //   list() {
+  //     console.log(this.list)
+  //   }
+  // }
 }
 </script>
 
--- demos/2020-9-28 Vue继续学/vuex/vuex-demo2/src/store/index.js
+++ demos/2020-9-28 Vue继续学/vuex/vuex-demo2/src/store/index.js
@@ -12,7 +12,9 @@ export default new Vuex.Store({
     // 文本框内容
     inputValue: 'aaa',
     // item的下一个id
-    nextId: 5
+    nextId: 5,
+    // viewKey，显示all还是已完成或未完成
+    viewKey: 'all'
   },
   mutations: {
     // 初始化list
@@ -37,6 +39,30 @@ export default new Vuex.Store({
       state.inputValue = ''
       // nextId自增
       state.nextId++
+    },
+    // 根据id删除item
+    removeItem(state, id) {
+      // 根据id找索引，在用索引删item
+      const i = state.list.findIndex(item => item.id === id)
+      // 如果i不等于-1说明找到了
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    // 修改item完成状态
+    changeStatus(state, param) {
+      const i = state.list.findIndex(item => item.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成
+    cleanDone(state) {
+      state.list = state.list.filter(x => !x.done)
+    },
+    // 切换viewKey
+    changeViewKey(state, key) {
+      state.viewKey = key
     }
   },
   actions: {
@@ -47,6 +73,27 @@ export default new Vuex.Store({
       axios.get('/list.json').then(({ data }) => {
         context.commit('initList', data)
       })
+    }
+  },
+  getters: {
+    // 统计未完成条数
+    // 首先state.list.filter过滤出未完成的item的数组，然后.length获取长度即可
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+
+    // 根据viewKey显示不同数组
+    filterList(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      return state.list
     }
   },
   modules: {}
