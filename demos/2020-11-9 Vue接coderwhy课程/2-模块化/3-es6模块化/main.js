90b6a877d6b9aeee746fe37d1d33b822ce81bf12
aoding9 <xy5217733@163.com>
2020-11-10 23:34:05 +0800

    改了改

--- demos/2020-11-9 Vue接coderwhy课程/2-模块化/3-es6模块化/main.js
+++ demos/2020-11-9 Vue接coderwhy课程/2-模块化/3-es6模块化/main.js
@@ -1,3 +1,11 @@
 // *用import 导入
 import { name, sing } from './moduleA.js'
 console.log(name, sing)
+
+import { age, dance, Person } from './moduleA.js'
+let ldh = new Person('刘德华', '18岁')
+console.log(age, dance, Person, ldh)
+
+// 默认导入，可以由导入者自己命名
+import myCity from './moduleA.js'
+console.log(myCity)
--- demos/2020-11-9 Vue接coderwhy课程/2-模块化/3-es6模块化/moduleA.js
+++ demos/2020-11-9 Vue接coderwhy课程/2-模块化/3-es6模块化/moduleA.js
@@ -2,5 +2,20 @@ const name = '雷军'
 function sing() {
   console.log('are you ok')
 }
-// *用export关键字导出
+// *1.第一种导出，导出变量，可以在导出的同时进行声明和赋值
+export let age = 18
+// *2.第二种导出，导出对象，在对象里面放要导出的内容
 export { name, sing }
+// *3，第三种导出，导出函数、类
+export function dance() {
+  console.log('在跳舞')
+}
+export class Person {
+  constructor(uname, age) {
+    this.uname = uname
+    this.age = age
+  }
+}
+// *4.export default默认导出，此时导入不需要使用对应的名称去解构赋值，而且可以由导入者自己命名
+const city = '湖南'
+export default city
